4c7ba4bcbd726485a394c47655361ceb359ce732
pacogp <paco.gomez@pedroortiz.com>
2021-04-03 22:43:20 +0200

    1 file modified, 1 deleted

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log('hello!')
+//Gráfico de columnas. Mi 1º gráfico
+var visualization = d3plus
+  .viz()
+  .container("#viz1")
+  .data([{"year": 1991, "name":"alpha", "value": 15},{"year": 1992, "name":"alpha", "value": 20},])
+  .type("bar")
+  .id("name")
+  .x("year")
+  .y("value")
+  .draw()
