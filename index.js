b31ea85589db62f204fc5bf70fd36a50d33f768f
pacogp <paco.gomez@pedroortiz.com>
2021-04-16 23:46:15 +0200

    1 file modified, 1 added, 1 deleted

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+//Gráfico de columnas. Mi 1º gráfico
+var visualization = d3plus .viz()
+  .container("#viz1")
+  .data([{"year": 1991, "name":"alpha", "value": 15},{"year": 1992, "name":"alpha", "value": 20},])
+  .type("bar")
+  .id("name")
+  .x("year")
+  .y("value")
+  .draw();
+
 var visualization = d3plus
     .viz()
     .container("#viz2")
@@ -28,16 +38,7 @@ var data2 = [
     {"year": 1992, "name":"delta2", "value": 35}
   ]
 
-//Gráfico de columnas. Mi 1º gráfico
-var visualization = d3plus
-  .viz()
-  .container("#viz1")
-  .data([{"year": 1991, "name":"alpha", "value": 15},{"year": 1992, "name":"alpha", "value": 20},])
-  .type("bar")
-  .id("name")
-  .x("year")
-  .y("value")
-  .draw()
+
 
   
 
